6c91f2e83833b332fda9820b8565cea97d005032
Tushar Saurabh <tusharacc@gmail.com>
2022-02-06 20:50:29 -0500

    Corrected CPP for Leetcode 7

--- cpp/leetcode_7.cpp
+++ cpp/leetcode_7.cpp
@@ -1,62 +1,61 @@
-#include <iostream>
+//Some Comment
 #include <vector>
+#include <iostream>
 #include <cmath>
 
 int reverse(int x){
-    std::vector<int> digits = {};
+    //std::vector<int> digits = {};
+    
+    int reverse = 0;
+    int num = std::abs(x);
+    int size = trunc(log10(num)) ;
+    
+    std::cout << "The number of digits" << size << std::endl;
 
     if (x <= -2147483648){
         return 0;
     }
 
-    int num = std::abs(x);
-
-    std::cout << "The original number" << x << std::endl;
-
     while (true){
-        std::cout << "the number is " << num << std::endl;
-        digits.push_back(num%10);
+    
+        int digit = num%10;
+
+        if (size >= 9 && digit >= 3){
+            return 0;
+        } else if (reverse > 2147483600 && digit >= 5){
+            return 0;
+        }
+        reverse = reverse + digit*pow(10,size);
+        size--;
         num = num /10;
+        
+        
         if (num == 0){
             break;
         }
     }
-    //std::cout << digits;
-    int reverse = 0;
-    int lengthOfDigits = digits.size()-1;
 
-    for (int i = 0; i < digits.size();i++){
-            std::cout << "The digits are" << reverse << "----"<< digits[i]  << std::endl;
-            if (lengthOfDigits >= 9 && digits[i] >= 3){
-                return 0;
-            } else if (reverse > 2147483600 && digits[i] >= 5 ) {
-                return 0;
-            }
-            else {
-                //std::cout << "The digits again " << digits[i] << lengthOfDigits << std::endl;
-                reverse = reverse + digits[i]*(pow(10,lengthOfDigits));
-                lengthOfDigits--;
-            }
-        }
-        
-        //std::cout << "The number" << reverse << std::endl;
+    std::cout << "The number" << reverse << std::endl;
 
-        //if (reverse >= -2147483648 and reverse <= 2147483647 ){
-        if (x == 0){
-            return reverse;
-        } else if (reverse < 0 && x > 0){
-            return 0;
-        }
-        else{
-            return (x/(abs(x)))*reverse;
-        }
+    //if (reverse >= -2147483648 and reverse <= 2147483647 ){
+    if (x == 0){
+        return 0;
+    } else if (reverse < 0 && x > 0){
+        return 0;
+    }
+    else{
+        return (x/(abs(x)))*reverse;
+    }
         
         
 }
 
 int main(int argc,char *argv[]){
     //std::cout << reverse(-2147483648);
-    std::cout << reverse(1563847412);
+    std::cout << reverse(1563847412) << std::endl;
+    std::cout << reverse(123456) << std::endl;
+    std::cout << reverse(1534236469) << std::endl;
+    std::cout << reverse(1563847412) << std::endl
 }
 
 //-2147483648
